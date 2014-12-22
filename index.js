f4c451075d3dcd847d07594409d7a03a20cdee4e
Jonathan Staats <jjstaats@gmail.com>
2014-12-22 16:42:18 +0100

    Added heroku database url

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
-var db = require('mongoskin').db('mongodb://localhost:27017/quiz', {safe: true});
+var db = require('mongoskin').db((process.env.MONGOLAB_URI || 'mongodb://localhost:27017/quiz'), {safe: true});
 
 var app = express();
+
+var stateName = 'state3';
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-db.bind('states')
+db.bind('states');
 
 app.get('/state', function (request, response) {
-
-    db.states.findOne({current:"state2"}, function(err, result) {
+    db.states.findOne({current:stateName}, function(err, result) {
         if (err) response.json({message:err.message});
 
         if (result != null)  {
             response.json(result.body);
         } else {
-            response.json({message:"Quiz master has not yet started!"})
+            response.json({message:'Quiz master has not yet started!'})
         }
 
     });
 });
 
 app.post('/state', function (request, response) {
-
-    db.states.findOne({current:"state2"}, function(err, result) {
+    db.states.findOne({current:stateName}, function(err, result) {
         if (err) response.json({message:err.message});
         if (result != null) {
             db.states.update({_id:result._id}, {$set:{body:request.body}}, function(err, result) {
@@ -36,7 +36,7 @@ app.post('/state', function (request, response) {
                 response.json(request.body);
             });
         } else {
-            db.states.insert({current:"state2", body:request.body}, function(err, result) {
+            db.states.insert({current:stateName, body:request.body}, function(err, result) {
                 if (err) response.json({message:err.message});
                 response.json(request.body);
             });
@@ -45,5 +45,5 @@ app.post('/state', function (request, response) {
 });
 
 app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'));
+    console.log('Node app is running at localhost:' + app.get('port'));
 });
