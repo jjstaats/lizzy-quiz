b21fad6ae102ce4f53cf2dcb3bf6e61b7e3459bd
Jonathan Staats <jjstaats@gmail.com>
2014-12-22 16:32:41 +0100

    added state calls

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var db = require('mongoskin').db('mongodb://localhost:27017/quiz', {safe: true});
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+db.bind('states')
+
+app.get('/state', function (request, response) {
+
+    db.states.findOne({current:"state2"}, function(err, result) {
+        if (err) response.json({message:err.message});
+
+        if (result != null)  {
+            response.json(result.body);
+        } else {
+            response.json({message:"Quiz master has not yet started!"})
+        }
+
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.post('/state', function (request, response) {
+
+    db.states.findOne({current:"state2"}, function(err, result) {
+        if (err) response.json({message:err.message});
+        if (result != null) {
+            db.states.update({_id:result._id}, {$set:{body:request.body}}, function(err, result) {
+                if (err) response.json({message:err.message});
+                response.json(request.body);
+            });
+        } else {
+            db.states.insert({current:"state2", body:request.body}, function(err, result) {
+                if (err) response.json({message:err.message});
+                response.json(request.body);
+            });
+        }
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
